3bd03b482c6e865070bf839c5076b168272af383
fabian <fabianmelendez321@gmail.com>
2025-02-13 17:51:00 -0500

    actividad1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Fabian Guillermo Melendez Gaitán");
+        System.out.println("Vivo en zipaquirá");
+
+        }
+    }
